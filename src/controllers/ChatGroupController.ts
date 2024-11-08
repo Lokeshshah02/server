60cdbcadca0d4d209aee48029bfcf255de98314e
Lokesh Shah <118357743+Lokeshshah02@users.noreply.github.com>
2024-11-08 22:05:41 +0530

    auth middleware, chatGroupe controller, prisma migrate custom-types.d.ts

---
+++ src/controllers/ChatGroupController.ts
@@ -0,0 +1,108 @@
+import { Request, Response } from "express"
+import prisma from "../config/db.config.js"
+
+class ChatGroupController {
+
+    static async index(req : Request, res : Response){
+        try{
+            const user = req.user
+
+            const groups =  await prisma.chatGroup.findMany({
+                where : {
+                    user_id : user.id,
+                },
+                orderBy : {
+                    created_at : "desc",
+                }
+            })
+
+            return res.json({message :  "Chat Groups fetched successfully", data : groups})
+
+        }catch(error){
+            return res.status(500).json({message :  "Something went wrong"})
+        }
+
+    }
+    static async delete(req : Request, res : Response){
+        try{
+            const {id} = req.params
+
+            await prisma.chatGroup.delete({
+                where : {
+                    id : id,
+                },
+                
+            })
+
+            return res.json({message :  "Chat Group deleted successfully"})
+
+        }catch(error){
+            return res.status(500).json({message :  "Something went wrong"})
+        }
+
+    }
+    static async show(req : Request, res : Response){
+        try{
+            const {id} = req.params
+
+            const groups =  await prisma.chatGroup.findUnique({
+                where : {
+                    id : id,
+                },
+                
+            })
+
+            return res.json({message :  "Chat Group fetched successfully", data : groups})
+
+        }catch(error){
+            return res.status(500).json({message :  "Something went wrong"})
+        }
+
+    }
+
+    static async update(req : Request, res : Response){
+        try{
+            const {id} = req.params
+            const body = req.body
+
+            await prisma.chatGroup.update({
+                data : {
+                    title : body.title,
+                    passcode : body.passcode,
+                },
+                where : {
+                    id : id
+                }
+            })
+
+            return res.json({message :  "Chat Group updated successfully"})
+
+        }catch(error){
+            return res.status(500).json({message :  "Something went wrong"})
+        }
+
+    }
+    static async store(req : Request, res : Response){
+        try{
+            const body = req.body
+            const user = req.user
+
+            await prisma.chatGroup.create({
+                data : {
+                    title : body.title,
+                    passcode : body.passcode,
+                    user_id : user.id
+                }
+            })
+
+            return res.json({message :  "Chat Group created successfully"})
+
+        }catch(error){
+            return res.status(500).json({message :  "Something went wrong"})
+        }
+
+    }
+
+}
+
+export default ChatGroupController
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController.js";
+import authMiddleware from "../middleware/AuthMiddleWare.js";
+import ChatGroupController from "../controllers/ChatGroupController.js";
 const router = Router()
 
+//Auth Routes
 router.post('/auth/login', AuthController.login)
+
+//Chat  Group routes
+router.post("/chat-group", authMiddleware, ChatGroupController.store)
+router.get("/chat-group", authMiddleware, ChatGroupController.index)
+router.get("/chat-group/:id", authMiddleware, ChatGroupController.show)
+router.put("/chat-group-update/:id", authMiddleware, ChatGroupController.update)
+router.delete("/chat-group/:id", authMiddleware, ChatGroupController.delete)
 
 export default router
