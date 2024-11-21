a35f6b2a780067895543c81635f41c57647d0b22
Lokesh Shah <118357743+Lokeshshah02@users.noreply.github.com>
2024-11-21 21:40:19 +0530

    docker installation and socket io working

--- dist/socket.js
+++ dist/socket.js
@@ -1,6 +1,10 @@
 export function setupSocket(io) {
     io.on("connection", (socket) => {
         console.log("The socket connect..", socket.id);
+        socket.on("message", (data) => {
+            console.log("Server side message", data);
+            socket.broadcast.emit("message", data);
+        });
         socket.on("disconnect", () => {
             console.log("A user disconnected", socket.id);
         });
--- src/socket.ts
+++ src/socket.ts
@@ -3,6 +3,11 @@ import { Server } from "socket.io";
 export function setupSocket( io : Server){
     io.on("connection", (socket)=> {
         console.log("The socket connect..",socket.id);
+
+        socket.on("message",(data)=>{
+            console.log("Server side message", data);
+            socket.broadcast.emit("message", data)            
+        })
         
         socket.on("disconnect", () =>{
             console.log("A user disconnected", socket.id);
