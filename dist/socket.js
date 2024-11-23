edc0a785868527f7720b3f95e52beba1f6d20134
Lokesh Shah <118357743+Lokeshshah02@users.noreply.github.com>
2024-11-23 21:57:10 +0530

    create rooms with socket io

--- dist/socket.js
+++ dist/socket.js
@@ -1,9 +1,21 @@
 export function setupSocket(io) {
+    io.use((socket, next) => {
+        const room = socket.handshake.auth.room || socket.handshake.headers.room;
+        if (!room) {
+            return next(new Error("Invalid room please pass the correct room id"));
+        }
+        socket.room = room;
+        next();
+    });
     io.on("connection", (socket) => {
+        // join room
+        socket.join(socket.room);
         console.log("The socket connect..", socket.id);
         socket.on("message", (data) => {
             console.log("Server side message", data);
-            socket.broadcast.emit("message", data);
+            // socket.broadcast.emit("message", data) 
+            // for room
+            io.to(socket.room).emit("message", data);
         });
         socket.on("disconnect", () => {
             console.log("A user disconnected", socket.id);
--- src/socket.ts
+++ src/socket.ts
@@ -1,12 +1,33 @@
-import { Server } from "socket.io";
+import { Server ,Socket } from "socket.io";
+
+interface CustomSocket extends Socket{
+    room?:string
+}
 
 export function setupSocket( io : Server){
-    io.on("connection", (socket)=> {
+
+    io.use((socket:CustomSocket ,next) => {
+
+        const room = socket.handshake.auth.room || socket.handshake.headers.room
+        if(!room){
+            return next(new Error("Invalid room please pass the correct room id"))
+        }
+        socket.room = room
+        next()
+    })
+
+    io.on("connection", (socket: CustomSocket)=> {
+
+        // join room
+        socket.join(socket.room)
+
         console.log("The socket connect..",socket.id);
 
         socket.on("message",(data)=>{
             console.log("Server side message", data);
-            socket.broadcast.emit("message", data)            
+            // socket.broadcast.emit("message", data) 
+           // for room
+           io.to(socket.room).emit("message", data);   
         })
         
         socket.on("disconnect", () =>{
